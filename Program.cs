9de38b339fadd04c7166198f2d84f85c6e05ff6c
Андрей яковлев <ssilver706@gmail.com>
2021-12-24 11:06:46 +0700

    добавили основу и вычисления

--- Program.cs
+++ Program.cs
@@ -1,12 +1,72 @@
 ﻿using System;
 
-namespace ConsoleApp1
+namespace lab5
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            voids func = new voids();
+            Console.WriteLine("Выполняемые функции:\n1)перевести дюймы в сантиметры\n2)Проверить число на четность(только целочисленные)\n3)Выбрать наибольшее число из массива целых чисел\n4)найти остаток от деления\n");
+            int i = int.Parse(Console.ReadLine());
+            switch (i)
+            {
+                case 1:
+                    Console.WriteLine("Введите число");
+                    try
+                    {
+                        string a = func.DtoS(double.Parse(Console.ReadLine()));
+                        Console.WriteLine(a);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Что-то пошло не так");
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Введите число");
+                    try
+                    {
+                        string b = func.chetnost(int.Parse(Console.ReadLine()));
+                        Console.WriteLine(b);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Что-то пошло не так");
+                    }
+                    break;
+                case 3:
+                    try
+                    {
+                        int col = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Введите числа:");
+                        int[] arr = new int[col];
+                        for (i = 0; i < col; i++)
+                            arr[i] = int.Parse(Console.ReadLine());
+                        string big_chislo = func.biggestnum(arr);
+                        Console.WriteLine("самое большое число:" + big_chislo);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    break;
+                case 4:
+                    try
+                    {
+                        Console.WriteLine("Введите первое число");
+                        int res1 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Введите второе число");
+                        int res2 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Остаток: " + func.ost(res1, res2));
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Что-то пошло не так");
+                    }
+                    break;
+            }
         }
     }
 }
---
+++ voids.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace lab5
+{
+    public class voids
+    {
+
+
+        public string DtoS(double a)
+        {
+            if (a < 0)
+                throw new ArgumentException("Число должно быть не отрицательным");
+            a *= 2.54;
+            return (a + " сантиметров");
+        }
+        public string chetnost(int a)
+        {
+            a %= 2;
+            if (a == 1 | a == -1)
+                return "число нечетное";
+            else
+                return "число четное";
+
+        }
+
+
+        public string biggestnum(int[] arr)
+        {
+            if (arr.Length == 0)
+                throw new ArgumentException("массив должен быть длиннее нуля");
+            int max = 0;
+            if (arr != null)
+                for (int i = 0; i < arr.Length; i++)
+                    if (arr[i] > max)
+                        max = arr[i];
+            string mm = max.ToString();
+            return mm;
+
+        }
+        public int ost(int a, int b)
+        {
+            if (b <= 0) throw new ArgumentException("Делитель должен быть >= 0");
+            return a % b;
+        }
+    }
+}
+
+
